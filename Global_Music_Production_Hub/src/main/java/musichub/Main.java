cfe47efec0ce4a0f9401f190ec6e9bc56bc5e933
jojomoment <kamiljamaljojo@gmail.com>
2026-03-08 15:56:08 +0000

    smaple table data made, testing in main

---
+++ Global_Music_Production_Hub/src/main/java/musichub/Main.java
@@ -0,0 +1,9 @@
+import musichub.domain.MusicTrack;
+
+public static void main(String[] args)
+{
+    // test object to see if everything's running
+    MusicTrack test = new MusicTrack(1, "Song One", 120, 180.0, null);
+
+    System.out.println(test);
+}
