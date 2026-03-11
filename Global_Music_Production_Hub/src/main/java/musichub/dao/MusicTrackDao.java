3045f7fcfa8f84a6f99d826a9e15b90be5cd2b45
jojomoment <kamiljamaljojo@gmail.com>
2026-03-11 00:17:41 +0000

    the methods i needed to do for stage 1

--- Global_Music_Production_Hub/src/main/java/musichub/dao/MusicTrackDao.java
+++ Global_Music_Production_Hub/src/main/java/musichub/dao/MusicTrackDao.java
@@ -2,6 +2,7 @@
 package musichub.dao;
 
 import musichub.domain.MusicTrack;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,13 +10,10 @@ import java.util.Optional;
 
 public interface MusicTrackDao { //defines interface, defines methods
 
-    MusicTrack insert(MusicTrack track); // adding new track to database
+    int insert(String songTitle, int BPM, double durationInSeconds) throws Exception; // adding new track to database
 
-    Optional<MusicTrack> findById(int id); // finds track by id, optional used instead of null, avoids crashing, can be used to highlight errors
+    List<MusicTrack> getAll() throws Exception;
 
-    List<MusicTrack> findAll(); // lists all the musicTrack objects
+    Optional<MusicTrack> getMusicTrackById(int songId) throws Exception;
 
-    boolean deleteById(int id); //deletes MusicTrack opbject and returns true if it actually got deleted
-
-    MusicTrack updateTrack(int id, MusicTrack track);//update/ replaces existing  track data with new data
-}
+    }
