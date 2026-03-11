4d6475723478b3ed2d12f8c5f3838d7d46fd1e50
jojomoment <kamiljamaljojo@gmail.com>
2026-03-11 00:16:25 +0000

    hard coded sample test

--- Global_Music_Production_Hub/src/main/java/musichub/Main.java
+++
@@ -1,9 +0,0 @@
-import musichub.domain.MusicTrack;
-
-public static void main(String[] args)
-{
-    // test object to see if everything's running
-    MusicTrack test = new MusicTrack(1, "Song One", 120, 180.0, null);
-
-    System.out.println(test);
-}
