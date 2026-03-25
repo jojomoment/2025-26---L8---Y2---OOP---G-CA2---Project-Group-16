078bc7dea97a7ac28d9f5b7ff1c98418d957e972
danielvin1 <acebtg0@gmail.com>
2026-03-25 19:02:40 +0000

    Implement JSON utility methods in MusicTrackJsonUtil
    Added methods for JSON serialization and deserialization of objects and lists.

--- Global_Music_Production_Hub/src/main/java/musichub/util/MusicTrackJsonUtil.java
+++ Global_Music_Production_Hub/src/main/java/musichub/util/MusicTrackJsonUtil.java
@@ -1,7 +1,32 @@
 package musichub.util;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
+
 public class MusicTrackJsonUtil {
-    public MusicTrackJsonUtil() {
-        // TODO Auto-generated method stub
+
+    private static final Gson gson = new Gson();
+
+   
+    public static <T> String toJson(T entity) {
+        return gson.toJson(entity);
+    }
+
+  
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+
+  
+    public static <T> String listToJson(List<T> list) {
+        return gson.toJson(list);
+    }
+
+    
+    public static <T> List<T> listFromJson(String json, Class<T> clazz) {
+        Type type = TypeToken.getParameterized(List.class, clazz).getType();
+        return gson.fromJson(json, type);
     }
 }
