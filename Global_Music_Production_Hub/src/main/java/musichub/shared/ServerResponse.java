19292c4ca9f8f64a98a10786c1c0c7bf345d71f3
danielvin1 <acebtg0@gmail.com>
2026-03-26 14:18:59 +0000

    Format ServerResponse class for consistency

--- Global_Music_Production_Hub/src/main/java/musichub/shared/ServerResponse.java
+++ Global_Music_Production_Hub/src/main/java/musichub/shared/ServerResponse.java
@@ -1,33 +1,40 @@
 package musichub.shared;
 
-public class ServerResponse<T> {
+public class ServerResponse<T>
+ {
     private boolean success;
     private String message;
     private T data;
 
-    private ServerResponse(boolean success, String message, T data) {
+    private ServerResponse(boolean success, String message, T data) 
+    {
         this.success = success;
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ServerResponse<T> ok(String message, T data) {
+    public static <T> ServerResponse<T> ok(String message, T data)
+     {
         return new ServerResponse<>(true, message, data);
     }
 
-    public static <T> ServerResponse<T> error(String message) {
+    public static <T> ServerResponse<T> error(String message) 
+    {
         return new ServerResponse<>(false, message, null);
     }
 
-    public boolean isSuccess() {
+    public boolean isSuccess() 
+    {
         return success;
     }
 
-    public String getMessage() {
+    public String getMessage() 
+    {
         return message;
     }
 
-    public T getData() {
+    public T getData() 
+    {
         return data;
     }
 }
